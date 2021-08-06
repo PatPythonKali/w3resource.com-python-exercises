4cd7437bf205e3a314db8fa4f00537287bdac3f2
ubuntu <patpyhtonkali@gmail.com>
2021-08-07 00:15:07 +0800

    Solution for question 3

---
+++ Excercise Solutions/question-3.py
@@ -0,0 +1,10 @@
+'''3. Write a Python program to sort a list of tuples using Lambda.
+Original list of tuples:
+[('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+Sorting the List of Tuples:
+[('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
+'''
+
+subjects = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+subjects.sort(key=lambda x:x[1])
+print(subjects)
